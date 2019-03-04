273de4e288277905a43571f2a9b9c077ad4eb199
Alejandro AR <43558224+alejandro10ar@users.noreply.github.com>
2019-03-04 11:24:28 -0500

    calculadora

---
+++ calculadora.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<!--&uacute; ESTE CODIGO ES PARA PONELE LAS TILDES A LAS VOCALES  A,E,I,O,U   --> 
+	<head>
+		<title>Operadores Aritm&eacute;ticos</title>
+	</head>
+	<body>
+		<h1>Calculadora</h1>
+		<form action="resultado.php" method="post">
+			<select name="operador">
+				<option value="suma">Suma</option>
+				<option value="resta">Resta</option>
+				<option value="multiplicacion">Multiplicaci&oacute;n</option>
+				<option value="division">Divisi&oacute;n</option>
+			</select><br />
+			Ingresa tu primer n&uacute;mero:<br />
+			<input type="text" name="valor1"><br />
+			Ingresa tu segundo valor:<br />
+			<input type="text" name="valor2"><br>
+			<input type="reset" value="Borrar">
+			<input type="submit" value="Enviar">
+		</form>
+	</body>
+</html>
